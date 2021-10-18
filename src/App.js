3cc746a5235ce1d7b76b8766be1b75615f975631
Léo Vetter <vetter.leo@gmail.com>
2021-10-18 12:51:39 +0200

    adding header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 class App extends Component {
   render() {
     return <div className='App'>
 
+      <Header />
       <Switch>
 
         <Route exact path='/' component={HomePage}/>
