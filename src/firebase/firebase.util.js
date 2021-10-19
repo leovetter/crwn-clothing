0a4d4ead975d1bccaefd4a0e0a7c5b57bbbd5b8b
Léo Vetter <vetter.leo@gmail.com>
2021-10-19 10:09:35 +0200

    added firebase file

---
+++ src/firebase/firebase.util.js
@@ -0,0 +1,9 @@
+// {
+//     apiKey: "AIzaSyCPuS02fn3z4ZGjagpU_cTGHytpbyez5vg",
+//     authDomain: "crwn-db-c9df9.firebaseapp.com",
+//     projectId: "crwn-db-c9df9",
+//     storageBucket: "crwn-db-c9df9.appspot.com",
+//     messagingSenderId: "72298035874",
+//     appId: "1:72298035874:web:3bcccd7695a45c52ebf5fb",
+//     measurementId: "G-1P5W8YDZXB"
+//   };
